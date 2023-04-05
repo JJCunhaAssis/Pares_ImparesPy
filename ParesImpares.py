5ac919c9bd9f8fed40407fbb1b840620a59da196
Jhonks <jvca_cunha@outlook.com>
2023-04-05 19:01:01 -0300

    AddParesImpares.py

---
+++ ParesImpares.py
@@ -0,0 +1,13 @@
+lista = [[], []]
+valor = 0
+for c in range(1, 8):
+    valor = int(input(f'Digite o {c}o. valor: '))
+    if valor % 2 == 0:
+        lista[0].append(valor)
+    else:
+        lista[1].append(valor)
+lista[0].sort()
+lista[1].sort()
+print('-=' * 30)
+print(f'Todos os valores {lista}')
+
